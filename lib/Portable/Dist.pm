a405704abb36770b295c0a27009ebe3a1bbf06d2
Adam Kennedy <adamk@cpan.org>
2008-06-04 04:12:13 +0000

    Importing the beginnings of Portable::Dist and Tie::Scalar::File

---
+++ lib/Portable/Dist.pm
@@ -0,0 +1,23 @@
+package Portable::Dist;
+
+=pod
+
+=head1 NAME
+
+Portable::Dist - Modify a Perl distribution to make it portable
+
+=head1 DESCRIPTION
+
+The L<Portable> family of modules provides functionality that allows
+Perl to operate from arbitrary and variable paths.
+
+B<Portable::Dist> is used to apply the necesary modifications to an
+existing Perl distribution.
+
+=head1 METHODS
+
+=cut
+
+use 5.008;
+use strict;
+use 
